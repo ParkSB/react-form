1b128dd5aa2912dfb661b33a04a652c47bfec91b
ParkSB <parkgds@gmail.com>
2018-12-24 22:39:37 +0900

    chore: 개발 환경 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "rules": {
+      "react/jsx-filename-extension": 0,
+      "arrow-body-style": 0,
+      "class-methods-use-this": 0,
+      "no-confusing-arrow": 0
+  },
+  "env": {
+      "browser": true
+  }
+};
